afcc0a54d97957fbd60322cc6ce5e0709dfe4b23
Ryan <ryan_walker184@hotmail.com>
2023-11-22 18:33:01 -0500

    Commit#2

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,19 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var passwordLength = 
   passwordText.value = password;
+
+  var passwordLength = prompt("Please choose the length of your generated password:");
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(passwordLength)
+  }
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
